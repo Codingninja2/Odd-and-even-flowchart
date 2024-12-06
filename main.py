8f3d1ab670dab0485e3d480f6efb1c2e55803631
codingninja2 <edwinafatsawo@gmail.com>
2024-12-06 19:46:25 +0000

    first commit

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ print("the result of 10/20 is",10/20)
 #subtracting two numbers
 print("the result of 10-20 is",10-20)
 #odd numbers
-print("1,3,5,7,9,11,13,15,17,19")
+print("odd numbers ,1,3,5,7,9,11,13,15,17,19")
 #even numbers
-print("2,4,6,8,10,12,14,16,18,20")
+print("even numbers ,2,4,6,8,10,12,14,16,18,20")
