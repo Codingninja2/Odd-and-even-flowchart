b319d915ef173c1a4600b13d6042ad26c789ef4d
codingninja2 <edwinafatsawo@gmail.com>
2024-12-06 19:44:30 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print("the result of 10+20 is",10+20)
+#multiplying two numbers
+print("the result of 10*20 is",10*20)
+#dividing numbers
+print("the result of 10/20 is",10/20)
+#subtracting two numbers
+print("the result of 10-20 is",10-20)
+#odd numbers
+print("1,3,5,7,9,11,13,15,17,19")
+#even numbers
+print("2,4,6,8,10,12,14,16,18,20")
